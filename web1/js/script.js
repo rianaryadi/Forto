67798f9cd9d1c484ad65e32ab30e8c5a666d4d78
Rian Aryadi <78772282+rianaryadi@users.noreply.github.com>
2021-02-09 10:10:38 +0700

    Update script.js

--- web1/js/script.js
+++ web1/js/script.js
@@ -1,4 +1,4 @@
-const txtElement = ['Freelancer', 'Web Developer', 'Graphic Designer'];
+const txtElement = ['Designer Custom', 'Illustrator'];
 let count = 0;
 let txtIndex = 0;
 let currentTxt = '';
